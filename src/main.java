38ccdf3a2e211806fba10eac0d024910444d4633
aafsari <amirafsari99@yahoo.com>
2018-10-08 18:26:30 +0330

    a new class made

--- src/main.java
+++ src/main.java
@@ -4,6 +4,9 @@ public class main {
     public class aaa{
         int a;
     }
+    public class bbb{
+        int a;
+    }
     public static void main(String[] args) {
         System.out.println("hi");
     }
