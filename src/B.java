a6982c33747799d7688f9f9cc677da8186c56b19
aafsari <amirafsari99@yahoo.com>
2018-10-11 15:21:23 +0330

    getter and setters added to class b

--- src/B.java
+++ src/B.java
@@ -3,4 +3,24 @@ public class B{
     private int a;
     private int b;
 
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+
+
 }
+
+
